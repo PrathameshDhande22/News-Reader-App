ef5392b26f9f22052f3e720de288eeb318e1d920
Prathamesh Dhande <prathameshdhande534@gmail.com>
2023-04-07 16:21:41 +0530

    new component footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+  return (
+    <div className="footer">
+      Developed by Lonith Shetty, Chaitanya Mestry, Goutam Thakur & Prathamesh
+      Dhande
+    </div>
+  );
+};
